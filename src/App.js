b5e655f09b310f782fca77c4d0bd69e09cd58b14
Vlad Kotu <vladimir.buhuru@gmail.com>
2020-03-10 13:46:36 +0200

    antd layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
 import React from 'react'
-import logo from './logo.svg'
+import { Layout } from 'antd'
+import logo from './images/logo.svg'
 import './App.css'
+
+const { Header, Footer, Content } = Layout
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout className='App-layout'>
+        <Header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1>Test React Feature Collections</h1>
+        </Header>
+        <Content className='App-content'>
+          <ul>
+            <li className='item001'>item 1</li>
+            <li className='item002'>item 2</li>
+            <li className='item003'>item 3</li>
+            <li className='item004'>item 4</li>
+            <li className='item005'>item 5</li>
+            <li className='item006'>item 6</li>
+            <li className='item007'>item 7</li>
+            <li className='item008'>item 8</li>
+            <li className='item009'>item 9</li>
+            <li className='item010'>item 10</li>
+            <li className='item011'>item 11</li>
+            <li className='item012'>item 12</li>
+            <li className='item013'>item 13</li>
+            <li className='item014'>item 14</li>
+            <li className='item015'>item 15</li>
+            <li className='item016'>item 16</li>
+            <li className='item017'>item 17</li>
+            <li className='item018'>item 18</li>
+            <li className='item019'>item 19</li>
+            <li className='item020'>item 20</li>
+            <li className='item021'>item 21</li>
+            <li className='item022'>item 22</li>
+            <li className='item023'>item 23</li>
+            <li className='item024'>item 24</li>
+            <li className='item025'>item 25</li>
+            <li className='item026'>item 26</li>
+            <li className='item027'>item 27</li>
+            <li className='item028'>item 28</li>
+            <li className='item029'>item 29</li>
+            <li className='item030'>item 30</li>
+            <li className='item031'>item 31</li>
+            <li className='item032'>item 32</li>
+            <li className='item033'>item 33</li>
+            <li className='item034'>item 34</li>
+            <li className='item035'>item 35</li>
+            <li className='item036'>item 36</li>
+            <li className='item037'>item 37</li>
+            <li className='item038'>item 38</li>
+            <li className='item039'>item 39</li>
+            <li className='item040'>item 40</li>
+            <li className='item041'>item 41</li>
+            <li className='item042'>item 42</li>
+            <li className='item043'>item 43</li>
+            <li className='item044'>item 44</li>
+            <li className='item045'>item 45</li>
+            <li className='item046'>item 46</li>
+            <li className='item047'>item 47</li>
+            <li className='item048'>item 48</li>
+            <li className='item049'>item 49</li>
+            <li className='item050'>item 50</li>
+            <li className='item051'>item 51</li>
+            <li className='item052'>item 52</li>
+            <li className='item053'>item 53</li>
+            <li className='item054'>item 54</li>
+            <li className='item055'>item 55</li>
+            <li className='item056'>item 56</li>
+            <li className='item057'>item 57</li>
+            <li className='item058'>item 58</li>
+            <li className='item059'>item 59</li>
+            <li className='item060'>item 60</li>
+            <li className='item061'>item 61</li>
+            <li className='item062'>item 62</li>
+            <li className='item063'>item 63</li>
+            <li className='item064'>item 64</li>
+            <li className='item065'>item 65</li>
+            <li className='item066'>item 66</li>
+            <li className='item067'>item 67</li>
+            <li className='item068'>item 68</li>
+            <li className='item069'>item 69</li>
+            <li className='item070'>item 70</li>
+            <li className='item071'>item 71</li>
+            <li className='item072'>item 72</li>
+            <li className='item073'>item 73</li>
+            <li className='item074'>item 74</li>
+            <li className='item075'>item 75</li>
+            <li className='item076'>item 76</li>
+            <li className='item077'>item 77</li>
+            <li className='item078'>item 78</li>
+            <li className='item079'>item 79</li>
+            <li className='item080'>item 80</li>
+            <li className='item081'>item 81</li>
+            <li className='item082'>item 82</li>
+            <li className='item083'>item 83</li>
+            <li className='item084'>item 84</li>
+            <li className='item085'>item 85</li>
+            <li className='item086'>item 86</li>
+            <li className='item087'>item 87</li>
+            <li className='item088'>item 88</li>
+            <li className='item089'>item 89</li>
+            <li className='item090'>item 90</li>
+            <li className='item091'>item 91</li>
+            <li className='item092'>item 92</li>
+            <li className='item093'>item 93</li>
+            <li className='item094'>item 94</li>
+            <li className='item095'>item 95</li>
+            <li className='item096'>item 96</li>
+            <li className='item097'>item 97</li>
+            <li className='item098'>item 98</li>
+            <li className='item099'>item 99</li>
+            <li className='item100'>item 100</li>
+          </ul>
+        </Content>
+        <Footer>@ Footer</Footer>
+      </Layout>
     </div>
   )
 }
