645da78e5ac39628c7869e86c398bf2e8cf8cc39
Vlad Kotu <vladimir.buhuru@gmail.com>
2020-03-12 17:54:14 +0200

    abstract fake api utils and create users api

---
+++ src/api/contractors.js
@@ -0,0 +1,18 @@
+import mocker from 'mocker-data-generator'
+import f from 'faker'
+import { makeFakeApi } from '../utils'
+
+const schema = {
+  id: { faker: 'random.number({"min": 10, "max": 100})' },
+  role: {
+    function: () =>
+      f.random.arrayElement(['Sales', 'Developer', 'Tech Lead', 'Assistant']),
+  },
+  salary: { faker: 'random.number({"min": 10000, "max": 100000})' },
+  fname: { faker: 'name.firstName' },
+  lname: { faker: 'name.lastName' },
+}
+
+export default {
+  contractors: makeFakeApi('contractors', schema),
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import mocker from 'mocker-data-generator'
 import f from 'faker'
 
 export const preventAndCall = (f, ...args) => ev => {
@@ -15,3 +16,55 @@ export const randomNumBut = (from, to, exclude = [], retries = 10) => {
     randomNumBut(from, to, exclude, --retries)
   }
 }
+
+// fake api utils
+export const getItems = ctx => JSON.parse(localStorage.getItem(ctx))
+export const storeItems = (ctx, ix) =>
+  localStorage.setItem(ctx, JSON.stringify(ix))
+
+export const makeFetchItems = (ctx, schema) => (n = 3) => {
+  const items = getItems(ctx)
+  if (items && items.length) return items
+  else
+    return mocker()
+      .schema(ctx, schema, n)
+      .build()
+      .then((data) => {
+        storeItems(ctx, data[ctx])
+        return data[ctx]
+      })
+}
+
+export const makeRemoveItem = ctx => item => {
+  storeItems(
+    ctx,
+    getItems(ctx).filter(({ id }) => item.id !== id)
+  )
+  return Promise.resolve(getItems(ctx))
+}
+
+export const makeAddItem = ctx => acc => {
+  const currentItems = getItems(ctx)
+  acc.id = randomNumBut(
+    10,
+    1000,
+    currentItems.map(i => i.id)
+  )
+  storeItems(ctx, [acc, ...currentItems])
+  return Promise.resolve(getItems(ctx))
+}
+
+export const makeUpdateItem = ctx => acc => {
+  const items = getItems(ctx)
+  const idx = items.findIndex(a => a.id === acc.id)
+  items[idx] = acc
+  storeItems(ctx, items)
+  return Promise.resolve(getItems(ctx))
+}
+
+export const makeFakeApi = (ctx, schema) => ({
+  addItem: makeAddItem(ctx),
+  fetchItems: makeFetchItems(ctx, schema),
+  updateItem: makeUpdateItem(ctx),
+  removeItem: makeRemoveItem(ctx),
+})
