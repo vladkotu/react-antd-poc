01b037226dafdff4c4662cd713bfd3aa8a82f510
Vlad Kotu <vladimir.buhuru@gmail.com>
2020-03-12 18:58:38 +0200

    create contractors form fields

--- src/pages/AccountsPage/formsConfig.js
+++ src/pages/AccountsPage/formsConfig.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { InputNumber, Input, Radio } from 'antd'
+import { InputNumber, Input, Radio, Select } from 'antd'
 import { BOOKEE, DEFACC, CONTRA } from '../../constants'
 
 const required = () => ({ required: true, message: 'This field is required' })
+
 const number = (min, max) => ({
   type: 'number',
   min: min,
   max: max,
   message: `This field accepts only numbers from ${min} to ${max}`,
 })
+
 const min = n => ({
   min: n,
   message: `Minimal length should be ${n} symbol${n === 1 ? '' : 's'}`,
@@ -72,13 +74,50 @@ const bookkeepingFields = [
   },
 ]
 
-const defaultFields = bookkeepingFields.slice(
-  0,
-  bookkeepingFields.length - 3
-)
+const defaultFields = bookkeepingFields.slice(0, bookkeepingFields.length - 3)
+
+const contractorFields = [
+  {
+    name: 'fname',
+    label: 'First Name',
+    field: <Input />,
+    rules: [required(), min(2), max(100)],
+  },
+
+  {
+    name: 'lname',
+    label: 'Last Name',
+    field: <Input />,
+    rules: [required(), min(2), max(100)],
+  },
+
+  {
+    name: 'role',
+    label: 'Role',
+    field: (
+      <Select
+        placeholder='Select a role'
+        allowClear
+      >
+        <Select.Option value='Sales'>Sales</Select.Option>
+        <Select.Option value='Developer'>Developer</Select.Option>
+        <Select.Option value='Tech Lead'>Tech Lead</Select.Option>
+        <Select.Option value='Assistant'>Assistant</Select.Option>
+        <Select.Option value='other'>other</Select.Option>
+      </Select>
+    ),
+  },
+
+  {
+    name: 'salary',
+    label: 'Salary',
+    field: <InputNumber />,
+    rules: [required(), number(10000, 100000)],
+  },
+]
 
 export default {
   [BOOKEE]: bookkeepingFields,
   [DEFACC]: defaultFields,
-  [CONTRA]: defaultFields
+  [CONTRA]: contractorFields,
 }
