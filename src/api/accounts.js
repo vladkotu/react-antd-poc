acdbd95100178b304b49570dc8ac07ea7eeb528f
Vlad Kotu <vladimir.buhuru@gmail.com>
2020-03-12 17:54:14 +0200

    abstract fake api utils and create users api

--- src/api/accounts.js
+++ src/api/accounts.js
@@ -1,6 +1,5 @@
-import mocker from 'mocker-data-generator'
 import f from 'faker'
-import { randomNumBut } from '../utils'
+import { makeFakeApi } from '../utils'
 
 const schema = {
   id: { faker: 'random.number({"min": 10, "max": 100})' },
@@ -20,58 +19,7 @@ const schema = {
   comment: { static: null },
 }
 
-// localStorage.removeItem('accounts')
-const getItems = ctx => JSON.parse(localStorage.getItem(ctx))
-const storeItems = (ctx, ix) => localStorage.setItem(ctx, JSON.stringify(ix))
-
-const makeFetchItems = ctx => (n = 3) => {
-  const accounts = getItems(ctx)
-  if (accounts && accounts.length) return accounts
-  else
-    return mocker()
-      .schema('acc', schema, n)
-      .build()
-      .then(({ acc }) => {
-        storeItems(ctx, acc)
-        return acc
-      })
-}
-
-const makeRemoveItem = ctx => account => {
-  storeItems(
-    ctx,
-    getItems(ctx).filter(({ id }) => account.id !== id)
-  )
-  return Promise.resolve(getItems(ctx))
-}
-
-const makeAddItem = ctx => acc => {
-  const currentItems = getItems(ctx)
-  acc.id = randomNumBut(
-    10,
-    1000,
-    currentItems.map(i => i.id)
-  )
-  storeItems(ctx, [acc, ...currentItems])
-  return Promise.resolve(getItems(ctx))
-}
-
-const makeUpdateItem = ctx => acc => {
-  const accounts = getItems(ctx)
-  const idx = accounts.findIndex(a => a.id === acc.id)
-  accounts[idx] = acc
-  storeItems(ctx, accounts)
-  return Promise.resolve(getItems(ctx))
-}
-
-const makeApi = ctx => ({
-  addItem: makeAddItem(ctx),
-  fetchItems: makeFetchItems(ctx),
-  updateItem: makeUpdateItem(ctx),
-  removeItem: makeRemoveItem(ctx),
-})
-
 export default {
-  bookkeepingAccounts: makeApi('bookkeepingAccounts'),
-  defaultAccounts: makeApi('defaultAccounts'),
+  bookkeepingAccounts: makeFakeApi('bookkeepingAccounts', schema),
+  defaultAccounts: makeFakeApi('defaultAccounts', schema),
 }
