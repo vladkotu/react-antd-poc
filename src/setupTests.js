2a0f7c8b24a649d4f507c933d182d24ec7ca8111
Vlad Kotu <vladimir.buhuru@gmail.com>
2020-03-18 16:32:15 +0200

    install enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,6 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16.1'
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
@@ -16,4 +14,6 @@ Object.defineProperty(window, 'matchMedia', {
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
   })),
-});
+})
+
+Enzyme.configure({ adapter: new Adapter() })
