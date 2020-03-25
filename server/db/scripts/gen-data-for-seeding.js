b6fef385cee2b6203d8e41e93cad06383cf314cc
Vlad Kotu <vladimir.buhuru@gmail.com>
2020-03-25 17:25:05 +0200

    move scripts to server/db folder as of their main purpose is db

--- server/db/scripts/gen-data-for-seeding.js
+++ server/db/scripts/gen-data-for-seeding.js
@@ -1,8 +1,9 @@
 import mocker from 'mocker-data-generator'
 import AWS from 'aws-sdk'
 import { promises as fs } from 'fs'
+import { v1 as uuidv1 } from 'uuid'
 import * as path from 'path'
-import { stripEmptyAttrs, isEmptyObj } from '../utils'
+import { stripEmptyAttrs } from '../../utils'
 import * as schemas from '../schemas'
 
 async function genData(ctx, schema, n) {
@@ -21,8 +22,11 @@ async function genData(ctx, schema, n) {
 export async function genStoreSeedData() {
   Object.entries(schemas).forEach(async ([ctx, schema]) => {
     const fdata = await genData(ctx, schema, 5)
+    console.log(`Generate data for ${ctx}`)
     const json = JSON.stringify(fdata, null, '  ')
-    const fpath = path.join(__dirname, '../../db/', `${ctx}-seed.json`)
+    const uuid = uuidv1()
+    const fpath = path.join(__dirname, '../', `${ctx}DataSeed${uuid}.json`)
+    console.log(`Store data to ${fpath}`)
     await fs.writeFile(fpath, json, { flag: 'w' })
   })
 }
